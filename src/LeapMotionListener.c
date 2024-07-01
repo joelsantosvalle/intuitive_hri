056fee438f3fe3179ced9e508666b8d2ee4b79e5
Joel Santos <105725933+joelsantosvalle@users.noreply.github.com>
2024-07-01 15:32:07 +0200

    Add files via upload

--- src/LeapMotionListener.c
+++ src/LeapMotionListener.c
@@ -1,4 +1,23 @@
-#include "LeapMotionListener.h"
+#include <LeapC.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+LEAP_CONNECTION* connection;
+LEAP_TRACKING_EVENT* tracking_event;
+struct sockaddr_in address;
+int udp_socket;
+LeapMotionListener;
+
+void on_init(LeapMotionListener* listener);
+void on_connect(LeapMotionListener* listener);
+void on_disconnect(LeapMotionListener* listener);
+void onExit(LeapMotionListener* listener);
+void on_frame(LeapMotionListener* listener, const LEAP_TRACKING_EVENT* frame);
 
 void on_init(LeapMotionListener* listener) {
     printf("Initialized\n");
