7db8afe5fbbbd110862c2c22e9a49da2b7f9aecb
Joel Santos <105725933+joelsantosvalle@users.noreply.github.com>
2024-07-01 17:13:45 +0200

    Add files via upload

--- src/LeapMotionListener.c
+++ src/LeapMotionListener.c
@@ -1,4 +1,4 @@
-#include <LeapC.h>
+#include <LeapC.h> //
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -86,23 +86,27 @@ void on_frame(LeapMotionListener* listener, const LEAP_TRACKING_EVENT* frame) {
 void main_loop(LeapMotionListener* listener) {
     while (1) {
         LEAP_CONNECTION_MESSAGE msg;
-        //if (LeapPollConnection(*listener->connection, 1000, &msg) == eLeapRS_Success) {
+        printf("1");
+        if (LeapPollConnection(*listener->connection, 1000, &msg) == eLeapRS_Success) {
             switch (msg.type) {
                 case eLeapEventType_Tracking:
+                            printf("3");
                     on_frame(listener, msg.tracking_event);
                     break;
                 default:
+                        printf("3");
+
                     break;
             }
-        //}
+        }
     }
 }
 
 int main() {
     LeapMotionListener listener;
     listener.connection = malloc(sizeof(LEAP_CONNECTION));
-    //LeapCreateConnection(NULL, listener.connection);
-    //LeapOpenConnection(*listener.connection);
+    LeapCreateConnection(NULL, listener.connection);
+    LeapOpenConnection(*listener.connection);
 
     listener.address.sin_family = AF_INET;
     listener.address.sin_port = htons(57410);
@@ -111,14 +115,14 @@ int main() {
     listener.udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
 
     on_init(&listener);
-    //on_connect(&listener);
+    on_connect(&listener);
 
     main_loop(&listener);
 
     onExit(&listener);
 
     close(listener.udp_socket);
-    //LeapDestroyConnection(*listener.connection);
+    LeapDestroyConnection(*listener.connection);
     free(listener.connection);
 
     return 0;
