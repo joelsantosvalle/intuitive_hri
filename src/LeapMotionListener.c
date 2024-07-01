52e7f1116fbd3dc0a23e08db0c34c918c20f1e86
Joel Santos <105725933+joelsantosvalle@users.noreply.github.com>
2024-07-01 12:51:50 +0200

    Sensor data updates

---
+++ src/LeapMotionListener.c
@@ -0,0 +1,102 @@
+#include "LeapMotionListener.h"
+
+void on_init(LeapMotionListener* listener) {
+    printf("Initialized\n");
+}
+
+void on_connect(LeapMotionListener* listener) {
+    printf("Motion Sensor Connected!\n");
+}
+
+void on_disconnect(LeapMotionListener* listener) {
+    printf("Motion sensor disconnected!\n");
+}
+
+void on_exit(LeapMotionListener* listener) {
+    printf("Exited\n");
+}
+
+void on_frame(LeapMotionListener* listener, const LEAP_TRACKING_EVENT* frame) {
+    int handnummer = frame->nHands;
+
+    if (handnummer < 1) {
+        float bytes[21] = {0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1};
+        sendto(listener->udp_socket, bytes, sizeof(bytes), 0, (struct sockaddr*)&listener->address, sizeof(listener->address));
+        usleep(100000);
+    } else {
+        for (uint32_t i = 0; i < frame->nHands; ++i) {
+            LEAP_HAND* hand = &frame->pHands[i];
+
+            const char* handType = hand->type == eLeapHandType_Left ? "Left Hand " : "Right Hand ";
+            printf("%sHand ID: %d Palm Position: (%f, %f, %f)\n", handType, hand->id, hand->palm.position.x, hand->palm.position.y, hand->palm.position.z);
+
+            float bytes[21] = {
+                (float)frame->nHands,
+                hand->grab_strength,
+                (float)hand->id,
+                hand->stabilized_palm.position.x,
+                hand->stabilized_palm.position.y,
+                hand->stabilized_palm.position.z,
+                hand->time_visible,
+                hand->palm.normal.y,
+                hand->palm.velocity.x,
+                hand->palm.velocity.y,
+                hand->palm.velocity.z,
+                hand->palm.basis.x_basis.x,
+                hand->palm.basis.x_basis.y,
+                hand->palm.basis.x_basis.z,
+                hand->palm.basis.y_basis.x,
+                hand->palm.basis.y_basis.y,
+                hand->palm.basis.y_basis.z,
+                hand->palm.basis.z_basis.x,
+                hand->palm.basis.z_basis.y,
+                hand->palm.basis.z_basis.z,
+                1
+            };
+
+            sendto(listener->udp_socket, bytes, sizeof(bytes), 0, (struct sockaddr*)&listener->address, sizeof(listener->address));
+            usleep(50000);
+        }
+    }
+}
+
+void main_loop(LeapMotionListener* listener) {
+    while (1) {
+        LEAP_CONNECTION_MESSAGE msg;
+        if (LeapPollConnection(*listener->connection, 1000, &msg) == eLeapRS_Success) {
+            switch (msg.type) {
+                case eLeapEventType_Tracking:
+                    on_frame(listener, msg.tracking_event);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
+
+int main() {
+    LeapMotionListener listener;
+    listener.connection = malloc(sizeof(LEAP_CONNECTION));
+    LeapCreateConnection(NULL, listener.connection);
+    LeapOpenConnection(*listener.connection);
+
+    listener.address.sin_family = AF_INET;
+    listener.address.sin_port = htons(57410);
+    listener.address.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    listener.udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
+
+    on_init(&listener);
+    on_connect(&listener);
+
+    main_loop(&listener);
+
+    on_exit(&listener);
+
+    close(listener.udp_socket);
+    LeapDestroyConnection(*listener.connection);
+    free(listener.connection);
+
+    return 0;
+}
---
+++ src/LeapMotionPublisher.cpp
@@ -0,0 +1,125 @@
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/float32.hpp>
+#include <geometry_msgs/msg/point.hpp>
+#include <std_msgs/msg/int32.hpp>
+#include <intuitive_hri/msg/orientation.hpp>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <cstring>
+#include <iostream>
+#include <array>
+
+using std::placeholders::_1;
+using namespace std::chrono_literals;
+
+class LeapDataNode : public rclcpp::Node
+{
+public:
+    LeapDataNode() : Node("leap_data")
+    {
+        pub_number_of_hands_ = this->create_publisher<std_msgs::msg::Float32>("HandNumber", 1);
+        pub_hand_state_ = this->create_publisher<std_msgs::msg::Float32>("hand_state", 1);
+        pub_hand_id_ = this->create_publisher<std_msgs::msg::Float32>("hand_id", 1);
+        pub_palm_position_stable_ = this->create_publisher<geometry_msgs::msg::Point>("hand_position_stable", 1);
+        pub_life_of_hand_ = this->create_publisher<std_msgs::msg::Float32>("life_of_hand", 1);
+        pub_hand_orientation_ = this->create_publisher<intuitive_hri::msg::Orientation>("hand_orientation", 1);
+        pub_hand_rate_of_change_ = this->create_publisher<geometry_msgs::msg::Point>("hand_rate_of_change", 1);
+
+        udp_socket_ = socket(AF_INET, SOCK_DGRAM, 0);
+        if (udp_socket_ < 0) {
+            perror("socket creation failed");
+            exit(EXIT_FAILURE);
+        }
+
+        struct sockaddr_in servaddr;
+        memset(&servaddr, 0, sizeof(servaddr));
+
+        servaddr.sin_family = AF_INET;
+        servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+        servaddr.sin_port = htons(57410);
+
+        if (bind(udp_socket_, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+            perror("bind failed");
+            close(udp_socket_);
+            exit(EXIT_FAILURE);
+        }
+
+        timer_ = this->create_wall_timer(
+            20ms, std::bind(&LeapDataNode::timer_callback, this));
+    }
+
+private:
+    void timer_callback()
+    {
+        std::array<float, 21> data;
+        struct sockaddr_in cliaddr;
+        socklen_t len = sizeof(cliaddr);
+        ssize_t n = recvfrom(udp_socket_, data.data(), data.size() * sizeof(float), MSG_WAITALL, (struct sockaddr *)&cliaddr, &len);
+        if (n < 0) {
+            perror("recvfrom failed");
+            return;
+        }
+
+        auto number_of_hand_in_frame = std_msgs::msg::Float32();
+        number_of_hand_in_frame.data = data[0];
+        pub_number_of_hands_->publish(number_of_hand_in_frame);
+
+        auto strength = std_msgs::msg::Float32();
+        strength.data = data[1];
+        pub_hand_state_->publish(strength);
+
+        auto hand_identifier = std_msgs::msg::Float32();
+        hand_identifier.data = data[2];
+        pub_hand_id_->publish(hand_identifier);
+
+        auto coordinates = geometry_msgs::msg::Point();
+        coordinates.x = data[3] * 0.001;
+        coordinates.y = data[4] * 0.001;
+        coordinates.z = data[5] * 0.001;
+        pub_palm_position_stable_->publish(coordinates);
+
+        auto life_of_hand_in_sensor = std_msgs::msg::Float32();
+        life_of_hand_in_sensor.data = data[6];
+        pub_life_of_hand_->publish(life_of_hand_in_sensor);
+
+        auto palm_direction = data[7]; // Unused in the publishers
+
+        auto rate_of_change = geometry_msgs::msg::Point();
+        rate_of_change.x = data[8];
+        rate_of_change.y = data[9];
+        rate_of_change.z = data[10];
+        pub_hand_rate_of_change_->publish(rate_of_change);
+
+        auto orientation_of_hand = human_robot_interaction::msg::Orientation();
+        orientation_of_hand.x_basis[0] = data[11];
+        orientation_of_hand.x_basis[1] = data[12];
+        orientation_of_hand.x_basis[2] = data[13];
+        orientation_of_hand.y_basis[0] = data[14];
+        orientation_of_hand.y_basis[1] = data[15];
+        orientation_of_hand.y_basis[2] = data[16];
+        orientation_of_hand.z_basis[0] = data[17];
+        orientation_of_hand.z_basis[1] = data[18];
+        orientation_of_hand.z_basis[2] = data[19];
+        pub_hand_orientation_->publish(orientation_of_hand);
+    }
+
+    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr pub_number_of_hands_;
+    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr pub_hand_state_;
+    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr pub_hand_id_;
+    rclcpp::Publisher<geometry_msgs::msg::Point>::SharedPtr pub_palm_position_stable_;
+    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr pub_life_of_hand_;
+    rclcpp::Publisher<human_robot_interaction::msg::Orientation>::SharedPtr pub_hand_orientation_;
+    rclcpp::Publisher<geometry_msgs::msg::Point>::SharedPtr pub_hand_rate_of_change_;
+
+    int udp_socket_;
+    rclcpp::TimerBase::SharedPtr timer_;
+};
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<LeapDataNode>());
+    rclcpp::shutdown();
+    return 0;
+}
